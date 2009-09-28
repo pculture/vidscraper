638a7c4002813385b956cd3b78f9993deac3b764
Paul Swartz <pswartz@pculture.org>
2009-09-28 15:03:08 -0400

    initial checkin for bulk importing support

---
+++ vidscraper/bulk_import/__init__.py
@@ -0,0 +1,37 @@
+import feedparser
+
+from vidscraper.bulk_import import opensearch
+
+IMPORTERS = (
+    opensearch,
+    )
+
+def bulk_import(feed_url):
+    """
+    Takes a URL for a feed, and returns a feedparser instance with the entries
+    filled in with all of the videos from that feed.  It'll take care of paging
+    through videos from services like blip.tv and Vimeo or services which
+    support OpenSearch (like YouTube).
+    """
+    parsed_feed = feedparser.parse(feed_url)
+    for importer in IMPORTERS:
+        if importer.video_count(parsed_feed) is not None:
+            return importer.bulk_import(parsed_feed)
+
+    # if we can't figure out how to bulk import the feed, just return the items
+    # in the feed
+    return parsed_feed
+
+
+def video_count(feed_url):
+    """
+    Takes a URL for a feed, and returns the number of videos we think we can
+    import from it.
+    """
+    parsed_feed = feedparser.parse(feed_url)
+    for importer in IMPORTERS:
+        count = importer.video_count(parsed_feed)
+        if count is not None:
+            return count
+
+    return len(parsed_feed.entries)
---
+++ vidscraper/bulk_import/opensearch.py
@@ -0,0 +1,36 @@
+import feedparser
+
+from vidscraper.bulk_import import util
+
+def _opensearch_get(parsed_feed, key):
+    # some versions of FeedParser strip off the opensearch_ prefix
+    return parsed_feed.feed.get(
+        'opensearch_%s' % key,
+        parsed_feed.feed.get(key, None))
+
+def video_count(parsed_feed):
+    """
+    Returns the number of videos that we think are in this feed in total.  If
+    the feed isn't a valid OpenSearch feed, return None.
+    """
+    if not (_opensearch_get(parsed_feed, 'startindex') and
+            _opensearch_get(parsed_feed, 'itemsperpage') and
+            _opensearch_get(parsed_feed, 'totalresults')):
+        return None # not a valid OpenSearch feed
+    return int(_opensearch_get(parsed_feed, 'totalresults'))
+
+def bulk_import(parsed_feed):
+    startindex = int(_opensearch_get(parsed_feed, 'startindex'))
+    itemsperpage = int(_opensearch_get(parsed_feed, 'itemsperpage'))
+    totalresults = int(_opensearch_get(parsed_feed, 'totalresults'))
+    feeds = []
+    for i in range(startindex, totalresults,
+                   itemsperpage):
+        if '?' in parsed_feed.href:
+            postfix = '&start-index=%i' % (i,)
+        else:
+            postfix = '?start-index=%i' % (i,)
+        feeds.append(feedparser.parse(parsed_feed.href + postfix))
+
+    return util.join_feeds(feeds)
+

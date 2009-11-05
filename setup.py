a2ba5ea961277bd3a63b4b112c05e2fc5f70d212
Paul Swartz <paulswartz@gmail.com>
2009-11-05 10:21:50 -0500

    add a basic setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+
+setup(name="vidscraper",
+      version=version,
+      author='Participatory Culture Foundation',
+      license='BSD')
+
+

cae8383b1dfb3887b7436a9385e8418ad3adeed2
Asheesh Laroia <asheesh@asheesh.org>
2011-04-22 13:59:50 -0400

    vidscraper now exports a bulk_import_url_list function
    This new function in the bulk_export API lets the caller get a list of feed URLs. Then
    the caller can take more control of the downloading process.

--- vidscraper/bulk_import/__init__.py
+++ vidscraper/bulk_import/__init__.py
@@ -9,6 +9,26 @@ IMPORTERS = (
     vimeo,
     )
 
+def bulk_import_url_list(parsed_feed):
+    """
+    Takes a parsed feed, and either:
+
+    * Returns a list of URLs that you can pass to feedparser, or
+    * Raises ValueError.
+
+    If you get ValueError, you should call bulk_import instead because the
+    IMPORTER that we chose does not support bulk_import_url_list.
+    """
+    for importer in IMPORTERS:
+        if importer.video_count(parsed_feed) is not None:
+            # Then this importer thinks it can provide data about the feed.
+            fn = getattr(importer, 'bulk_import_url_list', None)
+            if fn is not None:
+                return fn(parsed_feed)
+
+    raise ValueError, ("We cannot geneate a list of feed URLs for " +
+                       "the feed you gave us.")
+
 def bulk_import(feed_url, parsed_feed=None):
     """
     Takes a URL for a feed, and returns a feedparser instance with the entries

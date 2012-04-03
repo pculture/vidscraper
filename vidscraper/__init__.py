7fa4ab6d0eee978a1d2c5aa1a49b39347ef52d67
Stephen Burrows <stephen.r.burrows@gmail.com>
2012-04-03 04:05:31 -0700

    Corrections to vidscraper.__init__ to match the api changes.

--- vidscraper/__init__.py
+++ vidscraper/__init__.py
@@ -24,6 +24,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from vidscraper.exceptions import UnhandledSearch
 from vidscraper.suites import registry
 from vidscraper.videos import Video, VideoSearch, VideoFeed
 
@@ -64,19 +65,11 @@ def auto_scrape(url, fields=None, api_keys=None):
     return video
 
 
-def auto_feed(url, fields=None, crawl=False, max_results=None, api_keys=None,
-              last_modified=None, etag=None):
+def auto_feed(url, last_modified=None, etag=None, start_index=1,
+              max_results=None, video_fields=None, api_keys=None):
     """
-    Automatically determines which suite to use and scrapes ``feed_url`` with
-    that suite. This will return a :class:`VideoFeed` instance instantiated
-    using the determined suite. When iterated over, the :class:`VideoFeed`
-    will yield :class:`.Video` instances which have been initialized with
-    the given ``fields`` and ``api_keys``. If ``crawl`` is ``True`` (not the
-    default) then :mod:`vidscraper` will return results from multiple pages of
-    the feed, if the suite supports it.
-
-    .. note:: Crawling will only initiate a new HTTP request after it has
-              exhausted the results on the current page.
+    Tries to get a :class:`.VideoFeed` instance from each suite in sequence.
+    The parameters are the same as those for :class:`.VideoFeed`.
 
     :returns: A :class:`VideoFeed` instance which yields
               :class:`.Video` instances for the items in the feed.
@@ -84,29 +77,40 @@ def auto_feed(url, fields=None, crawl=False, max_results=None, api_keys=None,
     :raises UnhandledURL: if no registered suites know how to handle this url.
 
     """
-    return VideoFeed(url, fields=fields, crawl=crawl, max_results=max_results,
-                       api_keys=api_keys, last_modified=last_modified,
-                       etag=etag)
+    for suite in registry.suites:
+        try:
+            return suite.get_feed(url,
+                                  last_modified=last_modified,
+                                  etag=etag,
+                                  start_index=start_index,
+                                  max_results=max_results,
+                                  video_fields=video_fields,
+                                  api_keys=None)
+        except UnhandledURL:
+            pass
+    raise UnhandledURL
 
 
-def auto_search(query, fields=None, order_by=None, crawl=False,
-                max_results=None, api_keys=None):
+def auto_search(query, order_by=None, start_index=1, max_results=None,
+                video_fields=None, api_keys=None):
     """
     Returns a dictionary mapping each registered suite to a
     :class:`.VideoSearch` instance which has been instantiated for that suite
     and the given arguments.
 
     """
-    suites = {}
+    searches = {}
     for suite in registry.suites:
-        search = VideoSearch(query, suite, fields, order_by, crawl,
-                               max_results, api_keys)
         try:
-            search.get_first_url()
-        except NotImplementedError:
-            # suite doesn't implement search
+            search = suite.get_search(query,
+                                      order_by=order_by,
+                                      start_index=start_index,
+                                      max_results=max_results,
+                                      video_fields=video_fields,
+                                      api_keys=api_keys)
+        except UnhandledSearch:
             pass
         else:
-            suites[suite] = search
+            searches[suite] = search
         
-    return suites
+    return searches
